5df4d7b77b1c20d3f1c72d791ec10cf6e35522d2
AnnaV <anna@a-ventura.ru>
2023-08-14 18:10:34 +0300

    IPerson

---
+++ JAVAsem5/IPerson.java
@@ -0,0 +1,9 @@
+package JAVA_PROJECT.JAVAsem5;
+
+public interface IPerson {
+    String getName();
+    void setName(String name);
+    String getBirthDate();
+    void setBirthDate(String birthDate);
+}
+    
