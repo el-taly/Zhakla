b57c5dcca0654857448e67de0f68fb34bd24692f
AnnaV <anna@a-ventura.ru>
2023-08-14 18:08:16 +0300

    IGroup

---
+++ JAVAsem5/IGroup.java
@@ -0,0 +1,10 @@
+package JAVA_PROJECT.JAVAsem5;
+
+    public interface IGroup {
+        String getName();
+        void setName(String name);
+    
+        int getGroupNo();
+        void setGroupNo(int groupNo);
+    }
+
